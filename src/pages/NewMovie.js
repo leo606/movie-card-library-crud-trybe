e51e31c50f61913d9dcf3b8a3a3f5b37d17efe34
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-07-06 17:36:27 -0300

    requisito 6 - Insira um link na página inicial para `NewMovie` para criar novos cartões

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router';
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      movie: {
+        title: '',
+        subtitle: '',
+        storyline: '',
+        rating: 0,
+        imagePath: '',
+        bookmarked: false,
+        genre: '',
+      },
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // handleSubmit(newMovie) {
-  // }
+  async handleSubmit(newMovie) {
+    await movieAPI.createMovie(newMovie);
+    this.setState({ shouldRedirect: true });
+  }
 
   render() {
+    const { movie, shouldRedirect } = this.state;
+    if (shouldRedirect) return <Redirect to="/" />;
     return (
       <div data-testid="new-movie">
-        <MovieForm onSubmit={ this.handleSubmit } />
+        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
       </div>
     );
   }
